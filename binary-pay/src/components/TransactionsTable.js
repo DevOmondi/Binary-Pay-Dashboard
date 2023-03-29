254c95b06922bfa4b92e59fa4c567e55664447ce
olivermirimu <olivermirimu@gmail.com>
2023-03-29 13:50:35 +0300

    fix | fix bulk purchase

--- binary-pay/src/components/TransactionsTable.js
+++ binary-pay/src/components/TransactionsTable.js
@@ -1,119 +1,110 @@
-import * as React from 'react';
-import DataTable from './DataTable';
-import Box from '@mui/material/Box';
-import axios from 'axios';
+import * as React from "react";
+import DataTable from "./DataTable";
+import Box from "@mui/material/Box";
+import axios from "axios";
 import config from "../config";
-import { Button } from '@mui/material';
-import { useState } from 'react';
-import {jsPDF} from "jspdf";
-import 'jspdf-autotable';
+import { Button } from "@mui/material";
+import { useState } from "react";
+import { jsPDF } from "jspdf";
+import "jspdf-autotable";
 var XLSX = require("xlsx");
 
-
-
-
-
 const columns = [
-  { 
-    field: 'ref', 
-    headerName: 'REF', 
-    width: 160 
+  {
+    field: "ref",
+    headerName: "REF",
+    width: 160,
   },
   {
-    field: 'accountNumber',
-    headerName: 'Phone Number',
+    field: "accountNumber",
+    headerName: "Phone Number",
     width: 160,
     editable: true,
   },
   {
-    field: 'amount',
-    headerName: 'Amount',
+    field: "amount",
+    headerName: "Amount",
     width: 60,
     editable: true,
   },
   {
-    field: 'statusComplete',
-    headerName: 'Purchase Complete?',
+    field: "statusComplete",
+    headerName: "Purchase Complete?",
     width: 110,
     editable: true,
   },
   {
-    field: 'updatedAt',
-    headerName: 'Date',
-    type: 'date',
+    field: "updatedAt",
+    headerName: "Date",
+    type: "date",
     width: 160,
     editable: true,
   },
-  
 ];
 
-
 export default function TransactionsTable() {
-  const [transactions,setTransactions] = useState([])
+  const [transactions, setTransactions] = useState([]);
 
- const getTransactions =  async (e) => {
+  const getTransactions = async (e) => {
     e.preventDefault();
-    try{
-      const response = await axios.get(`${config.API_URL}/api/transaction/history`);
+    try {
+      const response = await axios.get(
+        `${config.API_URL}/api/transaction/history`
+      );
       const _data = response.data;
-    console.log(_data);
-    setTransactions(_data)
+      console.log(_data);
+      setTransactions(_data);
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-       console.log(error)
-    }  
-   
-  }
+  };
 
-   //exporting to excel sheet
- const downloadExcel = ()=>{
-  const workSheet = XLSX.utils.json_to_sheet(transactions);
-  const workBook = XLSX.utils.book_new();
+  //exporting to excel sheet
+  const downloadExcel = () => {
+    const workSheet = XLSX.utils.json_to_sheet(transactions);
+    const workBook = XLSX.utils.book_new();
 
-  XLSX.utils.book_append_sheet(workBook,workSheet,"Transactions");
-  //buffer
- XLSX.write(workBook,{bookType:"xlsx",type:"buffer"});
+    XLSX.utils.book_append_sheet(workBook, workSheet, "Transactions");
+    //buffer
+    XLSX.write(workBook, { bookType: "xlsx", type: "buffer" });
 
-  //binary
-  XLSX.write(workBook,{bookType:"xlsx", type:"binary"});
-  //download
-  XLSX.writeFile(workBook,"TransactionsData.xlsx")
-}
+    //binary
+    XLSX.write(workBook, { bookType: "xlsx", type: "binary" });
+    //download
+    XLSX.writeFile(workBook, "TransactionsData.xlsx");
+  };
 
-//exporting to pdf
-const downloadPDF = () => {
- const doc = new jsPDF();
- doc.text("Transaction Details",20,10);
+  //exporting to pdf
+  const downloadPDF = () => {
+    const doc = new jsPDF();
+    doc.text("Transaction Details", 20, 10);
 
- doc.autoTable({
-    columns:columns.map(col => ({...col,dataKey:col.id})),
-    body:transactions
-  })
-  doc.save("transactions.pdf");
-}
+    doc.autoTable({
+      columns: columns.map((col) => ({ ...col, dataKey: col.id })),
+      body: transactions,
+    });
+    doc.save("transactions.pdf");
+  };
 
-  
   return (
-    <Box sx={{ height: 800, width: '100%' }}>
+    <Box sx={{ height: 800, width: "100%" }}>
       <h1 id="transactions-title">Transactions</h1>
-       <form className='revenue-form'>
-                    <label>Start date :</label>
-                    <input typeof="date"></input>    
-                
-                    <label>End date :</label>
-                    <input typeof="date"></input>
-                    <button id='reload-btn' onClick={getTransactions}>Reload</button>
-        </form>
-        <form className='export-section'>
-                    <label>Export:</label>
-                    <button onClick={downloadExcel}>Excel</button>
-                    <button  onClick={downloadPDF}>PDF</button>
-                </form>
-      <DataTable
-        rows={transactions}
-        columns={columns}
-        
-      />
+      <form className="revenue-form">
+        <label>Start date :</label>
+        <input typeof="date"></input>
+
+        <label>End date :</label>
+        <input typeof="date"></input>
+        <button id="reload-btn" onClick={getTransactions}>
+          Reload
+        </button>
+      </form>
+      <form className="export-section">
+        <label>Export:</label>
+        <button onClick={downloadExcel}>Excel</button>
+        <button onClick={downloadPDF}>PDF</button>
+      </form>
+      <DataTable rows={transactions} columns={columns} />
     </Box>
   );
 }
